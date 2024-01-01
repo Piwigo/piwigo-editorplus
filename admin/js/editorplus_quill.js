5ee150cc6efda50c97cac48bfa63b1f007ba2811
Willy "Linty <lintydev@gmail.com>
2024-01-01 05:17:21 +0100

    formatting the code in editorplus_quill.js
    now we have a basis for developing the plugin and making clean code

--- admin/js/editorplus_quill.js
+++ admin/js/editorplus_quill.js
@@ -1,3 +1,19 @@
+// +-----------------------------------------------------------------------+
+// | EditorPlus : Quill                                                    |
+// |                                                                       |
+// | To avoid a large number of files, everything concerning quill editor  |
+// | is written in this file.                                              |
+// | Uppercase constants are defined in editorplus_quill.tpl.              |
+// | The file is divided into three parts:                                 |
+// |  - Definition of constants                                            |
+// |  - Definition of functions                                            |
+// |  - Display script                                                     |
+// +-----------------------------------------------------------------------+
+
+
+// +-----------------------------------------------------------------------+
+// | Definition of constants                                               |
+// +-----------------------------------------------------------------------+
 // Toolbar options
 const fontSizeArr = [false, '8px', '9px', '10px', '12px', '14px', '16px', '18px', '20px', '24px', '32px', '42px', '54px', '68px', '84px', '98px'];
 const toolbarOptions = [
@@ -10,6 +26,14 @@ const toolbarOptions = [
     ['link', 'image', 'video', 'blockquote'],
     ['clean'],
 ];
+
+// Editor container template
+const example_quill_container = `
+<div id="%CONTAINER_ID%" style="width:100%">
+    <iframe name="%QUILL_IFRAME_ID%" id="%QUILL_IFRAME_ID%" style="width:100%;height:%TEXTAREA_HEIGHT%" frameborder="0">
+    </iframe>
+</div>
+`;
 
 // Iframe template
 const example_quill_iframe = `
@@ -61,118 +85,222 @@ const example_quill_iframe = `
     window.onload = function() {
         const uniqueMessageType = "iframeLoaded_" + "%EDITOR_ID%"; 
         window.parent.postMessage({ type: uniqueMessageType }, '*');
+
+        window.addEventListener("keyup", (e) => {
+            if (e.key === 'Escape') {
+                const keyupMessageType = "iframeKeyup_" + "%EDITOR_ID%";
+                window.parent.postMessage({ type: keyupMessageType }, '*');
+            }
+        });
     };
 </` + `script>
 </html>`;
 
+// Toolbar fullscreen button template
+const example_fullscreen_button = `
+<span class="ql-formats icon-resize-full ep-icon" data-modal="inactive"></span>
+`;
+
+// +-----------------------------------------------------------------------+
+// | Definition of functions                                               |
+// +-----------------------------------------------------------------------+
+
+/**
+ * `EditorPlus - Quill` : Creates the editor iframe and displays it on the page.
+ * 
+ *  Hides the textarea and displays in its place an iframe that loads the Quill editor.
+ * @param textarea the textarea jquery object
+ * @returns quill_id, Quill, iframe_dom, quill
+ */
+function create_iframe_quill(textarea) {
+    try {
+        // Define iframe container constante
+        const quill_id = textarea.attr('id') ? textarea.attr('id') + '-quill' : textarea.attr('name') + '-quill';
+        const unique_iframe = new Date().getTime();  // Each time the page is loaded, we assign a new id to the iframe. This trick allows Safari not to cache the iframe (which causes problems).
+        const quill_iframe_id = quill_id + '-iframe' + unique_iframe;
+        const textarea_height = textarea.innerHeight() + 150; // + 150 is a good size for editor content
+        const quill_iframe = example_quill_container.replace(/%CONTAINER_ID%/g, 'container-' + quill_iframe_id).replace(/%QUILL_IFRAME_ID%/g, quill_iframe_id).replace(/%TEXTAREA_HEIGHT%/g, textarea_height + 'px');
+
+        // Hide textarea and display our editor
+        textarea.hide();
+        textarea.after(quill_iframe);
+
+        // Define iframe content constante
+        const iframe = $('#' + quill_iframe_id).get(0);
+        const iframe_dom = iframe.contentDocument || iframe.contentWindow.document; // For older browsers because they don't support "iframe.contentDocument".
+        const iframe_content = example_quill_iframe.replace(/%EDITOR_ID%/g, quill_id).replace(/%EP_PATH%/g, EP_PATH);
+
+        // Fill iframe content
+        iframe_dom.open();
+        iframe_dom.write(iframe_content);
+        iframe_dom.close();
+
+        // Return quill_id and Quill
+        return {
+            quill_id: quill_id,
+            Quill: iframe.contentWindow.Quill,
+            iframe_dom: iframe_dom,
+            quill: $(iframe_dom),
+            iframe_id: quill_iframe_id,
+        }
+    } catch (err) {
+        console.error('Unable to create iframe', err);
+    }
+}
+
+/**
+ * `EditorPlus - Quill` : Init and load quill editor in iframe
+ * 
+ * Setup quill with some config and add an expand/shrink button in toolbar
+ * @param {*} Quill Quill v2
+ * @param {*} iframe_dom iframe DOM Content
+ * @param {*} quill_id quill_id div
+ * @param {*} quill quill div jquery object
+ * @returns Quill, expand
+ */
+function load_quill(Quill, iframe_dom, quill_id, quill) {
+    try {
+        // We need inline style and not Quill CSS Class
+        const alignClass = Quill.import('attributors/style/align');
+        const backgroundClass = Quill.import('attributors/style/background');
+        const colorClass = Quill.import('attributors/style/color');
+        const fontClass = Quill.import('attributors/style/font');
+        const SizeClass = Quill.import('attributors/style/size');
+
+        SizeClass.whitelist = fontSizeArr;
+        Quill.register(alignClass, true);
+        Quill.register(backgroundClass, true);
+        Quill.register(colorClass, true);
+        Quill.register(fontClass, true);
+        Quill.register(SizeClass, true);
+
+        // Quill initialization
+        const quill_init = new Quill(iframe_dom.getElementById(quill_id), { // we use getElementById because for quill its better
+            modules: {
+                toolbar: toolbarOptions,
+            },
+            theme: 'snow',
+        });
+
+        // Add expand/shrink button and resize toolbar
+        const toolbar = quill.find('.ql-toolbar');
+        toolbar.append(example_fullscreen_button);
+        const iframe_ep_content = quill.find('.ep-content');
+        const toolbar_expand_button = toolbar.find('.ep-icon');
+
+        // !! maybe delete after make 2 toolbar !!
+        const toolbar_height = toolbar.innerHeight() + 2;
+        iframe_ep_content.css('height', 'calc(100% - ' + toolbar_height + 'px)');
+
+        return {
+            Quill: quill_init,
+            expand: toolbar_expand_button,
+        }
+    } catch (err) {
+        console.error('Unable to load quill', err);
+    }
+}
+
+/**
+ * `EditorPlus - Quill` : Show quill modal
+ */
+function show_quill_modal(quill_iframe_id, expand_button) {
+    try {
+        $('#container-' + quill_iframe_id).addClass('ep-modal-content');
+        $('#' + quill_iframe_id).addClass('ep-modal-iframe');
+        expand_button.removeClass('icon-resize-full');
+        expand_button.addClass('icon-resize-small');
+        expand_button.data('modal', 'active');
+    } catch (err) {
+        console.error('Unable to show quill modal', err);
+    }
+}
+
+/**
+ * `EditorPlus - Quill` : Close quill modal
+ */
+function close_quill_modal(quill_iframe_id, expand_button) {
+    try {
+        $('#container-' + quill_iframe_id).removeClass('ep-modal-content');
+        $('#' + quill_iframe_id).removeClass('ep-modal-iframe');
+        expand_button.removeClass('icon-resize-small');
+        expand_button.addClass('icon-resize-full');
+        expand_button.data('modal', 'inactive');
+    } catch (err) {
+        console.error('Unable to close modal', err);
+    }
+}
+
+// +-----------------------------------------------------------------------+
+// | Display script                                                        |
+// +-----------------------------------------------------------------------+
+
 // We load the editor
 // Waiting for DOM before load our script
 $(document).ready(function () {
-    
     // For each `EX_TEXTAREA` (textarea ids), we'll check whether we've found a textarea with these ids
-    $.each(EP_TEXTAREA, function(i, id) {
-        console.log(id);
+    // `EP_TEXTAREA` is define in editorplus_quill.tpl
+    $.each(EP_TEXTAREA, function (i, id) {
+        // Get textarea
         const textarea = $('#' + id);
 
-        // This is the most part of our script, we load the editor
+        // This is the most important part of our script, we load the editor
         if (textarea.length > 0 && textarea.is('textarea')) {
-            console.log(textarea);
+
             // On the album page, we remove the expand button from the description to leave the EditorPlus one
             if (EP_CURRENT_PAGE == 'album') { $('#desc-zoom-square').css('display', 'none'); }
-
-            // Define iframe const
-            const textarea_height = textarea.innerHeight() + 150; // 150 is a good size for editor content 
-            const quill_id = textarea.attr('id') ? textarea.attr('id') : textarea.attr('name') + '-quill';
-            const unique_iframe = new Date().getTime();  // Each time the page is loaded, we assign a new id to the iframe. This trick allows Safari not to cache the iframe (which causes problems).
-            const quill_iframe_id = quill_id + '-iframe' + unique_iframe;
-            const quill_iframe = '<div id="container-'+ quill_iframe_id +'" style="width:100%"><iframe name="' + quill_iframe_id + '" id="' + quill_iframe_id + '" style="width:100%;height:' + textarea_height + 'px" frameborder="0"></iframe></div>';
-        
-            // Hide textarea and display our editor (in iframe)
-            textarea.hide();
-            textarea.after(quill_iframe);
-
-            // Fill iframe
-            const iframe = $('#' + quill_iframe_id).get(0);
-            const iframe_quill = iframe.contentDocument || iframe.contentWindow.document; // For older browsers because they don't support "iframe.contentDocument".
-            const iframe_content = example_quill_iframe.replace(/%EDITOR_ID%/g, quill_id).replace(/%EP_PATH%/g, EP_PATH);
-
-            iframe_quill.open();
-            iframe_quill.write(iframe_content);
-            iframe_quill.close();
+            // create iframe
+            const iframe = create_iframe_quill(textarea);
 
             // We use an "addEventListener" which is triggered when the iframe dom is loaded and not "iframe.onload", 
             // because Safari handles onload differently.
             // Safari loads the editor only once and on only one page per session.
-            window.addEventListener('message', function (event) {
-                if (event.origin !== window.location.origin) return; // For security we check if the iframe have the same origin as piwigo server
-                if (event.data.type === 'iframeLoaded_' + quill_id) { // This is generate in iframe template
-                    const Quill = iframe.contentWindow.Quill;
-    
-                    // We need inline style and not Quill CSS Class
-                    const alignClass = Quill.import('attributors/style/align');
-                    const backgroundClass = Quill.import('attributors/style/background');
-                    const colorClass = Quill.import('attributors/style/color');
-                    const fontClass = Quill.import('attributors/style/font');
-                    const SizeClass = Quill.import('attributors/style/size');
-    
-                    SizeClass.whitelist = fontSizeArr;
-                    Quill.register(alignClass, true);
-                    Quill.register(backgroundClass, true);
-                    Quill.register(colorClass, true);
-                    Quill.register(fontClass, true);
-                    Quill.register(SizeClass, true);
-    
-                    // Init Quill
-                    const quill = new Quill(iframe_quill.getElementById(quill_id), { // we use getElementById because for quill its better
-                        modules: {
-                            toolbar: toolbarOptions,
-                        },
-                        theme: 'snow',
-                    });
-    
-                    // Add expand/shrink button and resize toolbar
-                    const i_quill = $(iframe_quill);
-                    const toolbar = i_quill.find('.ql-toolbar');
-                    const iframe_ep_content = i_quill.find('.ep-content');
-                    const fullScreen_button = $('<span class="ql-formats icon-resize-full ep-icon" data-modal="inactive"></span>');
-                    toolbar.append(fullScreen_button);
-    
-                    const toolbar_height = toolbar.innerHeight() + 2;
-                    iframe_ep_content.css('height', 'calc(100% - ' + toolbar_height + 'px)');
-    
-                    // Script for expand/shrink button
-                    toolbar.find('.ep-icon').on('click', function() {
-                        if($(this).data('modal') == 'inactive') {
-                            $('#container-' + quill_iframe_id).addClass('ep-modal-content');
-                            $('#' + quill_iframe_id).addClass('ep-modal-iframe');
-                            $(this).removeClass('icon-resize-full');
-                            $(this).addClass('icon-resize-small');
-                            $(this).data('modal', 'active');
+            window.addEventListener('message', function (e) {
+                // For security we check if the iframe have the same origin as piwigo server
+                if (e.origin !== window.location.origin) return;
+
+                // Iframe onload
+                if (e.data.type === 'iframeLoaded_' + iframe.quill_id) { // This is generate in example_quill_iframe
+                    // Init and load quill
+                    const quill = load_quill(iframe.Quill, iframe.iframe_dom, iframe.quill_id, iframe.quill);
+
+                    // Fill quill editor with textarea value
+                    quill.Quill.clipboard.dangerouslyPasteHTML(textarea.val());
+
+                    // addEventLister
+                    // Show/Hide quill modal
+                    quill.expand.on('click', function () {
+                        if (quill.expand.data('modal') == 'inactive') {
+                            show_quill_modal(iframe.iframe_id, quill.expand);
                         } else {
-                            $('#container-' + quill_iframe_id).removeClass('ep-modal-content');
-                            $('#' + quill_iframe_id).removeClass('ep-modal-iframe');
-                            $(this).removeClass('icon-resize-small');
-                            $(this).addClass('icon-resize-full');
-                            $(this).data('modal', 'inactive');
-                        } 
+                            close_quill_modal(iframe.iframe_id, quill.expand);
+                        }
                     });
-    
-                    // Quill and textarea value
-                    quill.clipboard.dangerouslyPasteHTML(textarea.val()); // Fill editor content with textarea value
-                    // On editor text change we fill the textarea with editor content
-                    quill.on('text-change', function () {
-                        textarea.val(quill.root.innerHTML);
+                    // On window click hide the modal
+                    $(window).on('click', function (e) {
+                        if (e.target == $('#container-' + iframe.iframe_id)[0]) {
+                            close_quill_modal(iframe.iframe_id, quill.expand);
+                        }
+                    });
+                    // On quill text-change we put his value in textarea
+                    quill.Quill.on('text-change', function () {
+                        textarea.val(quill.Quill.root.innerHTML);
                     });
                     // On textarea text change we fill the editor with textarea value
-                    textarea.on('change', function() {
-                        quill.clipboard.dangerouslyPasteHTML(textarea.val());
-                        console.log('je passe ici');
+                    textarea.on('change', function () {
+                        quill.Quill.clipboard.dangerouslyPasteHTML(textarea.val());
                     });
-                }
-            });
 
+                }
+
+                // Hide modal with escape key from iframe
+                if (e.data.type === 'iframeKeyup_' + iframe.quill_id) {
+                    const button = iframe.quill.find('.ep-icon'); // expand button in iframe
+                    close_quill_modal(iframe.iframe_id, button);
+                }
+
+            });
         }
 
     });
-
 });
