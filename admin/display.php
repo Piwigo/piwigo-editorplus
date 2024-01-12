44b7c0356079089e2e577528bb2d4f433239c3d4
Willy "Linty <lintydev@gmail.com>
2024-01-12 03:10:12 +0100

    replace ' by "
    for php performance

--- admin/display.php
+++ admin/display.php
@@ -1,4 +1,4 @@
 <?php
-if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+if (!defined("PHPWG_ROOT_PATH")) die("Hacking attempt!");
 
-$template->set_filename('editorplus_content', EP_REALPATH . '/admin/template/display.tpl');
+$template->set_filename("editorplus_content", EP_REALPATH . "/admin/template/display.tpl");
