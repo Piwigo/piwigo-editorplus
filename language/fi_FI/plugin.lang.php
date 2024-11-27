26ada97d21a9577912ba93d39fdd839d600738dc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-27 12:04:18 +0100

    Update fi_FI, thanks to maakuth

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'EditorPlussan pikkutilassa (pieni näyttö) voit valita muutamia toimintoja työkalupalkkiin.';
+$lang['Item is active in minify toolbar.'] = 'Toiminto on käytössä pikkutyökalupalkissa.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Toiminto on saatavilla raahattavaksi pikkutyökalupalkkiin.';
+$lang['Item is not available due to space restriction.'] = 'Toiminto ei ole saatavilla tilarajoituksen vuoksi.';
+$lang['Strike'] = 'Yliviivaus';
+$lang['Sub / Up'] = 'Ylä- tai alaindeksi';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Tältä editori näyttää Piwigossa.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Tehdäksesi näin, voit raahata toimintoja pikkutyökalupalkkiin.';
+$lang['Type something here'] = 'Kirjoita tähän jotain';
+$lang['Underline'] = 'Alleviivaus';
+$lang['Video'] = 'Video';
+$lang['Here you can play with it and see its features'] = 'Tässä sillä voi leikkiä ja nähdä sen ominaisuudet.';
+$lang['Italic'] = 'Kursiivi';
+$lang['Link'] = 'Linkki';
+$lang['Minify Toolbar (drag items here)'] = 'Pikkutyökalupalkki (raahaa kohteet tähän)';
+$lang['Quote'] = 'Lainaus';
+$lang['RTL'] = 'Oikealta vasemmalle';
+$lang['Sandbox'] = 'Hiekkalaatikko';
+$lang['Show/Hide Preview'] = 'Näytä/piilota esikatselu';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Tilarajoituksista johtuen et voi valita niitä kaikkia';
+$lang['Expand/Shrink Editor'] = 'Laajenna/kutista editori';
+$lang['Font size'] = 'Fonttikoko';
+$lang['Heading'] = 'Otsikko';
+$lang['Heading list'] = 'Otsikkolista';
+$lang['Clean'] = 'Poista muotoilut';
+$lang['Align'] = 'Tasaa';
+$lang['Background color'] = 'Taustaväri';
+$lang['Bold'] = 'Lihavoitu';
+$lang['Choose an editor'] = 'Valitse editori';
+$lang['Color'] = 'Väri';
+$lang['Direction'] = 'Suunta';
+$lang['EPImage'] = 'Kuva';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Font'] = 'Fontti';
