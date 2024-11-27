9dec2029f851d52462eccfda311e1e14bc5119d4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-27 12:04:13 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Item is active in minify toolbar.'] = '项目在迷你工具栏中处于活动状态。';
+$lang['Item is available for dragging in the minify toolbar.'] = '项目可在迷你工具栏中拖动。';
+$lang['Minify Toolbar (drag items here)'] = '迷你工具栏（将项目拖到此处）';
+$lang['To do this, drag and drop items into the minify toolbar.'] = '要执行此操作，请将项目拖放到迷你工具栏中。';
+$lang['Link'] = '链接';
+$lang['Quote'] = '引用';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = '沙盒';
+$lang['Show/Hide Preview'] = '显示/隐藏预览';
+$lang['Strike'] = '删除线';
+$lang['Sub / Up'] = '下标/上标';
+$lang['This is what the editor looks like throughout Piwigo.'] = '这是编辑器在Piwigo中的外观。';
+$lang['Type something here'] = '在这里输入内容';
+$lang['Underline'] = '下划线';
+$lang['Video'] = '视频';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = '使用EditorPlus简化模式（小显示），您可以选择一些项目添加到工具栏中。';
+$lang['Align'] = '对齐';
+$lang['Background color'] = '背景颜色';
+$lang['Bold'] = '粗体';
+$lang['Choose an editor'] = '选择编辑器';
+$lang['Clean'] = '清除';
+$lang['Color'] = '颜色';
+$lang['Direction'] = '方向';
+$lang['Due to the space restrictions you can\'t choose them all.'] = '由于空间限制，您不能选择所有选项。';
+$lang['EPImage'] = '图片';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = '展开/收缩编辑器';
+$lang['Font'] = '字体';
+$lang['Font size'] = '字体大小';
+$lang['Heading'] = '标题';
+$lang['Heading list'] = '标题列表';
+$lang['Here you can play with it and see its features'] = '您可以在这里试用并查看其功能';
+$lang['Italic'] = '斜体';
+$lang['Item is not available due to space restriction.'] = '由于空间限制，项目不可用。';
