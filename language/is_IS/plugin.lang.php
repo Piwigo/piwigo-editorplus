47abe5e500327779e5542f38d84da6fef7c25097
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-27 12:04:16 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Link'] = 'Krækja';
+$lang['Minify Toolbar (drag items here)'] = 'Minnka tækjastikan (dragðu hluti hingað)';
+$lang['Quote'] = 'Tilvitnun';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandkassi';
+$lang['Show/Hide Preview'] = 'Sýna/fela forskoðun';
+$lang['Strike'] = 'Eyða';
+$lang['Sub / Up'] = 'Undir / Upp';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Svona lítur ritillinn út um allt í Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Til að gera þetta skaltu draga og sleppa hlutum í minnka tækjastikunni.';
+$lang['Type something here'] = 'Skrifaðu eitthvað hér';
+$lang['Underline'] = 'Undirstrika';
+$lang['Video'] = 'Myndband';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Með EditorPlus minnka ham (lítill skjár) geturðu valið nokkra hluti til að bæta við tækjastikuna.';
+$lang['Align'] = 'Jafna';
+$lang['Background color'] = 'Bakgrunnslitur';
+$lang['Bold'] = 'Feitletrað';
+$lang['Choose an editor'] = 'Veldu ritil';
+$lang['Clean'] = 'Hreinsa';
+$lang['Color'] = 'Litur';
+$lang['Direction'] = 'Stefna';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Vegna plásstakmarkana geturðu ekki valið þá alla.';
+$lang['EPImage'] = 'Mynd';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Stækka/minnka ritil';
+$lang['Font'] = 'Leturgerð';
+$lang['Font size'] = 'Leturstærð';
+$lang['Heading'] = 'Fyrirsögn';
+$lang['Heading list'] = 'Fyrirsagnarlisti';
+$lang['Here you can play with it and see its features'] = 'Hér geturðu leikið þér með það og séð eiginleika þess';
+$lang['Italic'] = 'Skáletrað';
+$lang['Item is active in minify toolbar.'] = 'Atriði er virkt í minnka tækjastikunni.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Hlutur er tiltækur til að draga í minnka tækjastikunni.';
+$lang['Item is not available due to space restriction.'] = 'Varan er ekki fáanleg vegna plásstakmarkana.';
