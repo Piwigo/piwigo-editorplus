a9bf010a3bb119ce674177101ac5b654b391a21e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-27 12:04:10 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Expand/Shrink Editor'] = 'Utvid/Krymp redigeringsprogram';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Elementet er tilgjengelig for dra i minimert-verktøylinje.';
+$lang['Item is active in minify toolbar.'] = 'Elementet er aktivt i minimert-verktøylinje.';
+$lang['Link'] = 'Link';
+$lang['Minify Toolbar (drag items here)'] = 'Minimert verktøylinje (dra elementer hit)';
+$lang['Quote'] = 'Sitere';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandkasse';
+$lang['Show/Hide Preview'] = 'Vis/Skjul Forhåndsvisning';
+$lang['Strike'] = 'Streik';
+$lang['Sub / Up'] = 'Sub / Opp';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Slik ser redigeringsprogrammet ut i hele Piwigo';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'For å gjøre dette, dra og slipp elementer inn i minimerte-verktøylinjen.';
+$lang['Type something here'] = 'Skriv noe her';
+$lang['Underline'] = 'Understrek';
+$lang['Video'] = 'Video';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Med Redigeringsprogrammet Plus minimert-modus (liten skjerm), kan du velge noen få elementer du vil legge til på verktøylinjen.';
+$lang['Align'] = 'Justere';
+$lang['Background color'] = 'Bakgrunnsfarge';
+$lang['Bold'] = 'Fet';
+$lang['Choose an editor'] = 'Velg et redigeringsprogram';
+$lang['Clean'] = 'Rengjør';
+$lang['Color'] = 'Farge';
+$lang['Direction'] = 'Retning';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'På grunn av plassbegrensningene kan du ikke velge alle.';
+$lang['EPImage'] = 'Bilde';
+$lang['EditorPlus'] = 'Redigeringsprogram Pluss';
+$lang['Font'] = 'Skrift';
+$lang['Font size'] = 'Skriftstørrelse';
+$lang['Heading'] = 'Overskrift';
+$lang['Heading list'] = 'Overskriftsliste';
+$lang['Here you can play with it and see its features'] = 'Her kan du leke med den og se funksjonene';
+$lang['Italic'] = 'Italic';
+$lang['Item is not available due to space restriction.'] = 'Gjenstanden er ikke tilgjengelig på grunn av plassbegrensning.';
