9a0b4a264055692496c6b6ef749efed6d93d70c6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-27 08:26:03 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Bold'] = 'Mutig';
+$lang['Underline'] = 'Unterstreichen Sie';
+$lang['Video'] = 'Video';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Im EditorPlus-Minimierungsmodus (kleine Anzeige) können Sie einige Elemente auswählen, die der Symbolleiste hinzugefügt werden sollen.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'So sieht der Editor in Piwigo aus.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Ziehen Sie dazu Elemente per Drag & Drop in die Minify-Symbolleiste.';
+$lang['Type something here'] = 'Geben Sie hier etwas ein';
+$lang['Show/Hide Preview'] = 'Vorschau anzeigen/verbergen';
+$lang['Strike'] = 'Streik';
+$lang['Sub / Up'] = 'Ab / Auf';
+$lang['Link'] = 'Link';
+$lang['Minify Toolbar (drag items here)'] = 'Symbolleiste verkleinern (Elemente hierher ziehen)';
+$lang['Quote'] = 'Zitat';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandbox';
+$lang['Italic'] = 'Kursiv';
+$lang['Item is active in minify toolbar.'] = 'Das Element ist in der Minify-Symbolleiste aktiv.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Das Element ist in der Minify-Symbolleiste zum Ziehen verfügbar.';
+$lang['Item is not available due to space restriction.'] = 'Der Artikel ist aus Platzgründen nicht verfügbar.';
+$lang['Font'] = 'Schriftart';
+$lang['Font size'] = 'Schriftgröße';
+$lang['Heading'] = 'Überschrift';
+$lang['Heading list'] = 'Überschriftenliste';
+$lang['Here you can play with it and see its features'] = 'Hier kannst du damit spielen und seine Funktionen sehen';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Aus Platzgründen können Sie nicht alle auswählen.';
+$lang['EPImage'] = 'Bild';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Erweitern/Schrumpfen Editor';
+$lang['Clean'] = 'Sauber';
+$lang['Color'] = 'Farbe';
+$lang['Direction'] = 'Richtung';
+$lang['Align'] = 'Ausrichten';
+$lang['Background color'] = 'Hintergrundfarbe';
+$lang['Choose an editor'] = 'Wählen Sie einen Editor';
