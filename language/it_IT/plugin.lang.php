097ce1908c35f5b7e407ced33c184e28c227ea4d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-28 12:25:48 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sub / Up'] = 'Sotto / Sopra';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Così è come appare l\'editor in Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Per farlo, trascina e rilascia gli elementi nella barra degli strumenti di minimizzazione.';
+$lang['Type something here'] = 'Digita qualcosa qui';
+$lang['Underline'] = 'Sottolineato';
+$lang['Video'] = 'Video';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Avviso: se cambi editor, alcuni elementi di formattazione non saranno compatibili, e ciò potrebbe comportare una perdita dello stile all\'interno dei tuoi contenuti.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Con la modalità minimizzazione (display piccolo) di EditorPlus, puoi scegliere alcuni elementi da aggiungere alla barra degli strumenti.';
+$lang['Strike'] = 'Barrato';
+$lang['Link'] = 'Collegamento';
+$lang['Minify Toolbar (drag items here)'] = 'Riduci barra degli strumenti (trascina gli elementi qui)';
+$lang['Quote'] = 'Quota';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandbox';
+$lang['Show/Hide Preview'] = 'Mostra/nascondi anteprima';
+$lang['Align'] = 'Allinea';
+$lang['Background color'] = 'Colore di sfondo';
+$lang['Bold'] = 'Grassetto';
+$lang['Choose an editor'] = 'Seleziona un editor';
+$lang['Clean'] = 'Pulisci';
+$lang['Color'] = 'Colore';
+$lang['Direction'] = 'Direzione';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'A causa delle restrizioni sullo spazio, non puoi sceglierli tutti.';
+$lang['EPImage'] = 'Immagine';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Espandi/restringi editor';
+$lang['Font'] = 'Font';
+$lang['Font size'] = 'Dimensione font';
+$lang['Heading'] = 'Intestazione';
+$lang['Heading list'] = 'Elenco intestazioni';
+$lang['Here you can play with it and see its features'] = 'Qui puoi giocarci e vedere le sue caratteristiche';
+$lang['Italic'] = 'Corsivo';
+$lang['Item is active in minify toolbar.'] = 'L\'elemento è attivo nella barra degli strumenti minimizzata.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'L\'elemento è disponibile per il trascinamento nella barra degli strumenti di minimizzazione.';
+$lang['Item is not available due to space restriction.'] = 'L\'elemento non è disponibile a causa di limitazioni di spazio.';
