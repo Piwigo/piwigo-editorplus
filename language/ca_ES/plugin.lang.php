559549cf23a59169ac95fea0e0df8eda525ea293
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-28 06:46:52 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Align'] = 'Alinea';
+$lang['Background color'] = 'Color de rerefons';
+$lang['Bold'] = 'Negreta';
+$lang['Choose an editor'] = 'Escolliu un editor';
+$lang['Clean'] = 'Neteja';
+$lang['Color'] = 'Color';
+$lang['Direction'] = 'Direcció';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'No podeu escollir-los tots degut a restriccions d\'espai';
+$lang['EPImage'] = 'Imatge';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Expandir/contraure l\'Editor';
+$lang['Font'] = 'Tipografia';
+$lang['Font size'] = 'Mida de la tipografia';
+$lang['Heading'] = 'Capçalera';
+$lang['Heading list'] = 'Llista de capçaleres';
+$lang['Here you can play with it and see its features'] = 'Aquí podeu jugar amb ell i veure\'n les característiques';
+$lang['Italic'] = 'Cursiva';
+$lang['Item is active in minify toolbar.'] = 'L\'element està actiu a la barra d\'eines de minimització.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'L\'element està disponible per arrossegar-lo a la barra d\'eines de minimització.';
+$lang['Item is not available due to space restriction.'] = 'L\'element no està disponible degut a restriccions d\'espai.';
+$lang['Link'] = 'Enllaç';
+$lang['Minify Toolbar (drag items here)'] = 'Barra d\'eines de minimització (arrossegueu elements aquí)';
+$lang['Quote'] = 'Cometes';
+$lang['RTL'] = 'RTL (de dreta a esquerra)';
+$lang['Sandbox'] = 'Caixa de sorra';
+$lang['Show/Hide Preview'] = 'Mostra/amaga la vista prèvia';
+$lang['Strike'] = 'Tatxat';
+$lang['Sub / Up'] = 'Sots-índex/sobre-índex';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Així es veu l\'editor a través de Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Per fer això, arrossegueu i deixeu anar elements a la barra d\'eines de minimització.';
+$lang['Type something here'] = 'Escriviu alguna cosa aquí';
+$lang['Underline'] = 'Sots-ratllat';
+$lang['Video'] = 'Vídeo';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Advertiment: Si canvieu d\'editor, alguns elements de format no seran compatibles, cosa que podria resultar en la pèrdua de l\'estil del vostre contingut.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Amb el mode de minimització (pantalla petita) de l\'EditorPlus, podeu escollir alguns elements per afegir a la barra d\'eines.';
