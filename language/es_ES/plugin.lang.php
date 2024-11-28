16fa1274d13a952f0c90f345de09cfee01278356
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-28 06:46:55 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Align'] = 'Alinear';
+$lang['Background color'] = 'Color de fondo';
+$lang['Bold'] = 'Negrita';
+$lang['Choose an editor'] = 'Elija un editor';
+$lang['Clean'] = 'Limpiar';
+$lang['Color'] = 'Color';
+$lang['Direction'] = 'Dirección';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'No puede elegirlos todos debido a restricciones de espacio.';
+$lang['EPImage'] = 'Imagen';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Expandir/contraer Editor';
+$lang['Font'] = 'Tipografía';
+$lang['Font size'] = 'Tamaño de la tipografía';
+$lang['Heading'] = 'Cabecera';
+$lang['Heading list'] = 'Lista de cabeceras';
+$lang['Here you can play with it and see its features'] = 'Aquí puede jugar con él y ver sus características';
+$lang['Italic'] = 'Cursiva';
+$lang['Item is active in minify toolbar.'] = 'El elemento está activo en la barra de herramientas de minimización.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'El elemento está disponible para arrastrarlo a la barra de herramientas de minimización.';
+$lang['Item is not available due to space restriction.'] = 'El elemento no está disponible debido a restricciones de espacio.';
+$lang['Link'] = 'Enlace';
+$lang['Minify Toolbar (drag items here)'] = 'Barra de herramientas de minimización (arrastre elementos aquí)';
+$lang['Quote'] = 'Comillas';
+$lang['RTL'] = 'RTL (de derecha a izquierda)';
+$lang['Sandbox'] = 'Cajón de arena';
+$lang['Show/Hide Preview'] = 'Mostrar/ocultar vista previa';
+$lang['Strike'] = 'Tachado';
+$lang['Sub / Up'] = 'Subíndice/sobreíndice';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Así es como se ve el editor a través de Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Para hacer esto, arrastre y suelte elementos en la barra de herramientas de minimización.';
+$lang['Type something here'] = 'Escriba algo aquí';
+$lang['Underline'] = 'Subrayado';
+$lang['Video'] = 'Vídeo';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Advertencia: Si cambia de editor, algunos elementos de formato no serán compatibles, lo que podría resultar en la pérdida del estilo de su contenido.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Con el modo de minimización (pantalla pequeña) de EditorPlus, puede elegir algunos elementos que añadir a la barra de herramientas.';
