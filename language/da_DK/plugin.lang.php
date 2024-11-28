0888726b1b784c17a4239003d8bb2e09c99856b2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-28 08:14:26 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'For at gøre dette, trækkes og slippes emner på minify-værktøjslinjen.';
+$lang['Font size'] = 'Skriftstørrelse';
+$lang['Sub / Up'] = 'Indeks / potens';
+$lang['Link'] = 'Link';
+$lang['Minify Toolbar (drag items here)'] = 'Minify-værktøjslinjen (træk emner hertil)';
+$lang['Quote'] = 'Citér';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandkasse';
+$lang['Show/Hide Preview'] = 'Vis/skjul forhåndsvisning';
+$lang['Strike'] = 'Overstreg';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Sådan ser editoren ud gennem Piwigo.';
+$lang['Type something here'] = 'Skriv noget her';
+$lang['Underline'] = 'Understregning';
+$lang['Video'] = 'Video';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Advarsel: Hvis du ændrer editoren, vil nogle formateringselementer ikke være kompatible, hvilket kan medføre at dine stilvalg mistes.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Med EditorPlus i minify-tilstand (lille skærm), kan du vælge nogle få emner, som føjes til værktøjslinjen.';
+$lang['Align'] = 'Justér';
+$lang['Background color'] = 'Baggrundsfarve';
+$lang['Bold'] = 'Fed';
+$lang['Choose an editor'] = 'Vælg en editor';
+$lang['Clean'] = 'Rens';
+$lang['Color'] = 'Farve';
+$lang['Direction'] = 'Retning';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Pga. pladsbegrænsninger kan du ikke vælge dem alle.';
+$lang['EPImage'] = 'Billede';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Udvid/formindsk editor';
+$lang['Font'] = 'Skrifttype';
+$lang['Heading'] = 'Overskrift';
+$lang['Heading list'] = 'Overskriftsliste';
+$lang['Here you can play with it and see its features'] = 'Her kan du eksperimentere og se funktionerne';
+$lang['Italic'] = 'Kursiv';
+$lang['Item is active in minify toolbar.'] = 'Emnet er aktivt i minify-værktøjslinjen.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Emnet er tilgængeligt til at trække i minify-værktøjslinjen.';
+$lang['Item is not available due to space restriction.'] = 'Emnet er ikke tilgængeligt pga. pladsbegrænsninger.';
