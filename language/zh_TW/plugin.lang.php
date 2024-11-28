81ad042900a4c732152932cf8004032313fe0fc4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-28 12:25:52 +0100

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = '使用 EditorPlus 的迷你模式，您可以自行選項幾個要加入工具列中的項目。';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = '警告：若是變更編輯器的設定，有些格式化元素可能會不相容，導致現有內容的格式跑掉。';
+$lang['Item is active in minify toolbar.'] = '在迷你工具列中存在';
+$lang['Item is available for dragging in the minify toolbar.'] = '在迷你工具列中可拉動';
+$lang['Link'] = '連結';
+$lang['Minify Toolbar (drag items here)'] = '迷你工具列（將項目拖拉至此處）';
+$lang['Quote'] = '引文';
+$lang['RTL'] = '由右至左';
+$lang['Sandbox'] = '沙盒';
+$lang['Show/Hide Preview'] = '顯示/隱藏預覽';
+$lang['Strike'] = '刪除線';
+$lang['Sub / Up'] = '上/下標';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Piwigo 中所有的編輯都會長得像這樣';
+$lang['To do this, drag and drop items into the minify toolbar.'] = '要這麼做的話，請將項目拖放到迷你工具列上。';
+$lang['Type something here'] = '在這兒打些字吧';
+$lang['Underline'] = '底線';
+$lang['Video'] = '影片';
+$lang['Item is not available due to space restriction.'] = '由於空間限制，無法選用';
+$lang['Italic'] = '斜體';
+$lang['Expand/Shrink Editor'] = '縮放編輯器';
+$lang['Font'] = '字型';
+$lang['Font size'] = '字體大小';
+$lang['Heading'] = '標題';
+$lang['Heading list'] = '標題列表';
+$lang['Here you can play with it and see its features'] = '您可以在此試用並查看它的功能';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['EPImage'] = '圖片';
+$lang['Due to the space restrictions you can\'t choose them all.'] = '由於空間的限制，不是所有選項都能選用';
+$lang['Direction'] = '方向';
+$lang['Color'] = '色彩';
+$lang['Clean'] = '清除';
+$lang['Choose an editor'] = '選擇編輯器';
+$lang['Bold'] = '粗體';
+$lang['Background color'] = '背景色彩';
+$lang['Align'] = '對齊';
