72a8fc1130fffe3d69dc0d2a28defc0e0bd355fb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-29 11:46:19 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sub / Up'] = 'Alt / Üst';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = '%s yapılandırması devre dışı. EditorPlus\'ı kullanmak için bu yapılandırmayı etkinleştirmelisiniz.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Piwigo\'da editörün görünümü bu şekildedir.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Bunu yapmak için öğeleri küçültme araç çubuğuna sürükleyip bırakın.';
+$lang['Type something here'] = 'Buraya bir şey yazın';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Uyarı: Editörü değiştirirseniz bazı biçimlendirme öğeleri uyumlu olmayacaktır ve bu da içeriğinizde stil kaybına neden olabilir.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'EditorPlus minify modu (küçük ekran) ile araç çubuğuna eklemek üzere birkaç öğe seçebilirsiniz.';
+$lang['Strike'] = 'Çarp';
+$lang['Link'] = 'Bağlantı';
+$lang['Minify Toolbar (drag items here)'] = 'Araç Çubuğunu Küçült (öğeleri buraya sürükleyin)';
+$lang['Quote'] = 'Alıntı';
+$lang['RTL'] = 'Sağdan sola';
+$lang['Sandbox'] = 'Kum havuzu';
+$lang['Show/Hide Preview'] = 'Önizlemeyi Göster/Gizle';
+$lang['Underline'] = 'Altı çizili';
+$lang['Video'] = 'Video';
+$lang['Align'] = 'Hizala';
+$lang['Item is active in minify toolbar.'] = 'Öğe, küçültme araç çubuğunda etkin.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Öğe, küçültme araç çubuğunda sürüklenebilir.';
+$lang['Item is not available due to space restriction.'] = 'Ürün yer kısıtlaması nedeniyle mevcut değil.';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Yer kısıtlaması nedeniyle hepsini seçemezsiniz.';
+$lang['EditorPlus'] = 'EditörPlus';
+$lang['Expand/Shrink Editor'] = 'Editörü Genişlet/Küçült';
+$lang['Choose an editor'] = 'Bir editör seçin';
+$lang['Bold'] = 'Kalın';
+$lang['Background color'] = 'Arkaplan rengi';
+$lang['Clean'] = 'Temizle';
+$lang['Direction'] = 'Yön';
+$lang['Color'] = 'Renk';
+$lang['Here you can play with it and see its features'] = 'Burada onunla oynayabilir ve özelliklerini görebilirsiniz';
+$lang['Italic'] = 'İtalik';
+$lang['EPImage'] = 'Resim';
+$lang['Font size'] = 'Yazı tipi boyutu';
+$lang['Font'] = 'Yazı tipi';
+$lang['Heading'] = 'Başlık';
+$lang['Heading list'] = 'Başlık listesi';
