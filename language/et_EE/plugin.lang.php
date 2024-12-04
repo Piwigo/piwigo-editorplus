76d7ab8d98c9e0061effae7d78deb1292e86e887
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-04 18:22:28 +0100

    Update et_EE, thanks to Exabot

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Liivakast';
+$lang['Show/Hide Preview'] = 'Kuva/peida eelvaade';
+$lang['Strike'] = 'Strke';
+$lang['Sub / Up'] = 'Alam/üles';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = 'Konfiguratsioon %s on keelatud. EditorPlusi kasutamiseks peate selle konfiguratsiooni lubama.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Selline näeb toimetaja kogu Piwigo välja.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Selleks lohistage üksused minimeerimise tööriistaribale.';
+$lang['Type something here'] = 'Sisestage midagi siia';
+$lang['Underline'] = 'Allajoonimine';
+$lang['Video'] = 'Video';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Hoiatus. Kui muudate redaktorit, ei ühildu mõned vorminguelemendid, mis võib kaasa tuua stiilist sisu kadumise.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'EditorPlusi minimeerimisrežiimiga (väike ekraan) saate valida mõne üksuse, mida tööriistaribale lisada.';
+$lang['Link'] = 'Link';
+$lang['Minify Toolbar (drag items here)'] = 'Tööriistariba minimeerimine (lohistage üksused siia)';
+$lang['Quote'] = 'Tsiteeri';
+$lang['Expand/Shrink Editor'] = 'Laienda/kahanda redaktorit';
+$lang['Font'] = 'Font';
+$lang['Font size'] = 'Fondi suurus';
+$lang['Heading'] = 'Pealkiri';
+$lang['Heading list'] = 'Pealkirjade loend';
+$lang['Here you can play with it and see its features'] = 'Siin saate sellega mängida ja selle funktsioone näha';
+$lang['Italic'] = 'Kursiiv';
+$lang['Item is active in minify toolbar.'] = 'Üksus on minimeerimise tööriistaribal aktiivne.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Üksus on minimeerimise tööriistaribal lohistamiseks saadaval.';
+$lang['Item is not available due to space restriction.'] = 'Toode pole ruumipiirangu tõttu saadaval.';
+$lang['Color'] = 'Värv';
+$lang['Direction'] = 'Suund';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Ruumipiirangute tõttu ei saa kõiki valida.';
+$lang['EPImage'] = 'Pilt';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Bold'] = 'Paks';
+$lang['Clean'] = 'Puhas';
+$lang['Choose an editor'] = 'Valige redaktor';
+$lang['Background color'] = 'Taustavärv';
+$lang['Align'] = 'Joonda';
