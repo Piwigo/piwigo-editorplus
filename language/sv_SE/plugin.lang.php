fe1738fc320827f78324ecb96c1b2aa219dd4506
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-19 06:55:48 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sub / Up'] = 'Nedsänkt / Upphöjd';
+$lang['Link'] = 'Länk';
+$lang['Minify Toolbar (drag items here)'] = 'Minify-verktygsfältet (dra objekt hit)';
+$lang['Quote'] = 'Citat';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandlåda';
+$lang['Show/Hide Preview'] = 'Visa/göm förhandsvisning';
+$lang['Strike'] = 'Genomstruken';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = 'Konfigurationen %s är inaktiverad. För att använda EditorPlus måste du aktivera denna konfiguration.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Så ser redigeraren ut i hela Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'För att göra detta, dra och släpp objekt i minify-verktygsfältet.';
+$lang['Type something here'] = 'Skriv någonting här';
+$lang['Underline'] = 'Understruken';
+$lang['Video'] = 'Video';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Varning: Om du byter redigerare kommer vissa formateringselement inte att vara kompatibla, vilket kan leda till att ditt innehåll tappar stil.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Med EditorPlus minifieringsläge (liten skärm) kan du välja några objekt att lägga till i verktygsfältet.';
+$lang['Align'] = 'Justering';
+$lang['Background color'] = 'Bakgrundsfärg';
+$lang['Bold'] = 'Fet';
+$lang['Choose an editor'] = 'Välj en redigerare';
+$lang['Clean'] = 'Rensa';
+$lang['Color'] = 'Färg';
+$lang['Direction'] = 'Riktning';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'P.g.a. utrymmesbegränsning så kan du inte välja alla.';
+$lang['EPImage'] = 'Bild';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Expandera/krymp redigerare';
+$lang['Font'] = 'Typsnitt';
+$lang['Font size'] = 'Typsnittsstorlek';
+$lang['Heading'] = 'Rubrik';
+$lang['Heading list'] = 'Rubriklista';
+$lang['Here you can play with it and see its features'] = 'Här kan du leka med den och se dess funktioner';
+$lang['Italic'] = 'Kursiv';
+$lang['Item is active in minify toolbar.'] = 'Objektet är aktivt i minify-verktygsfältet.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Objektet är tillgängligt för att dra i minify-verktygsfältet.';
+$lang['Item is not available due to space restriction.'] = 'Objektet är inte tillgängligt p.g.a. utrymmesbegränsning.';
