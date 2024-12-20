7242736e7a928e3c28406a485e75d2c5c7f7252a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:54:43 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Link'] = 'קישור';
+$lang['Minify Toolbar (drag items here)'] = 'צמצם סרגל הכלים (גרור פריטים לכאן)';
+$lang['Quote'] = 'ציטוט';
+$lang['RTL'] = 'ימין לשמאל';
+$lang['Sandbox'] = 'חולייה';
+$lang['Show/Hide Preview'] = 'הצג/הסתר תצוגה מקדימה';
+$lang['Strike'] = 'קו חוצה';
+$lang['Sub / Up'] = 'משני / למעלה';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = 'התצורה %s מנוטרלת. כדי להשתמש ב-EditorPlus עליך להפעיל את התצורה הזו.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'זה כיצד העורך נראה בכל רחבי Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'כדי לעשות זאת, גרר ושחרר פריטים לתוך סרגל הכלים.';
+$lang['Type something here'] = 'הקלד משהו כאן';
+$lang['Underline'] = 'קו תחתון';
+$lang['Video'] = 'וידאו';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'אזהרה: אם תשנה את העורך, חלק מרכיבי העיצוב לא יהיו תואמים, וזה עשוי לגרום לאובדן סגנון בתוכן שלך.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'עם מצב צמצום ב- EditorPlus (תצוגה קטנה), תוכל לבחור כמה פריטים להוספה לסרגל הכלים.';
+$lang['Align'] = 'יישור';
+$lang['Background color'] = 'צבע רקע';
+$lang['Bold'] = 'מודגש';
+$lang['Choose an editor'] = 'בחר עורך';
+$lang['Clean'] = 'נקה';
+$lang['Color'] = 'צבע';
+$lang['Direction'] = 'כיוון';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'עקב המגבלות במרחב, אינך יכול לבחור את כולם.';
+$lang['EPImage'] = 'תמונה';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'הרחב/צמצם עורך';
+$lang['Font'] = 'גופן';
+$lang['Font size'] = 'גודל גופן';
+$lang['Heading'] = 'כותרת';
+$lang['Heading list'] = 'רשימת כותרות';
+$lang['Here you can play with it and see its features'] = 'כאן תוכל לשחק עם זה ולראות את התכונות שלו';
+$lang['Italic'] = 'נטוי';
+$lang['Item is active in minify toolbar.'] = 'הפריט פעיל בסרגל הכלים לדחיסה.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'הפריט זמין לגרירה בסרגל הכלים לדחיסה.';
+$lang['Item is not available due to space restriction.'] = 'הפריט אינו זמין עקב המגבלות במרחב.';
