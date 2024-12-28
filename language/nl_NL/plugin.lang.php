875617b59d03e806dbfde0d06f45063b499d7cd6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-28 15:09:05 +0100

    Update nl_NL, thanks to navisence

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Align'] = 'Uitlijnen';
+$lang['Background color'] = 'Achtergrondkleur';
+$lang['Bold'] = 'Vet';
+$lang['Choose an editor'] = 'Kies een editor';
+$lang['Color'] = 'Kleur';
+$lang['Direction'] = 'Richting';
+$lang['EPImage'] = 'Afbeelding';
