4a3855b98bbc8daeeec3a3dccc6131df31883712
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-02-22 05:11:13 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Link'] = 'Link';
+$lang['Minify Toolbar (drag items here)'] = 'Barra de ferramentas Minify (arraste os itens aqui)';
+$lang['Quote'] = 'Citar';
+$lang['RTL'] = 'Direto ao ponto';
+$lang['Sandbox'] = 'Caixa de areia';
+$lang['Show/Hide Preview'] = 'Mostrar/Ocultar Visualização';
+$lang['Strike'] = 'Batida';
+$lang['Sub / Up'] = 'Sub / Para cima';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = 'A configuração %s está desabilitada. Para usar o EditorPlus você deve habilitar esta configuração.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'É assim que o editor se parece no Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Para fazer isso, arraste e solte itens na barra de ferramentas de redução.';
+$lang['Type something here'] = 'Digite algo aqui';
+$lang['Underline'] = 'Sublinhado';
+$lang['Video'] = 'Vídeo';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Aviso: Se você mudar de editor, alguns elementos de formatação não serão compatíveis, o que pode resultar em perda de estilo no seu conteúdo.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Com o modo de redução do EditorPlus (tela pequena), você pode escolher alguns itens para adicionar à barra de ferramentas.';
+$lang['Align'] = 'Alinhar';
+$lang['Background color'] = 'Cor de fundo';
+$lang['Bold'] = 'Audacioso';
+$lang['Choose an editor'] = 'Escolha um editor';
+$lang['Clean'] = 'Limpar';
+$lang['Color'] = 'Cor';
+$lang['Direction'] = 'Direção';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Devido às restrições de espaço, você não pode escolher todos.';
+$lang['EPImage'] = 'Imagem';
+$lang['EditorPlus'] = 'Editor Plus';
+$lang['Expand/Shrink Editor'] = 'Expandir/Reduzir Editor';
+$lang['Font'] = 'Fonte';
+$lang['Font size'] = 'Tamanho da fonte';
+$lang['Heading'] = 'Cabeçalho';
+$lang['Heading list'] = 'Lista de cabeçalhos';
+$lang['Here you can play with it and see its features'] = 'Aqui você pode brincar com ele e ver suas características';
+$lang['Italic'] = 'itálico';
+$lang['Item is active in minify toolbar.'] = 'O item está ativo na barra de ferramentas de redução.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'O item está disponível para ser arrastado na barra de ferramentas de redução.';
+$lang['Item is not available due to space restriction.'] = 'O item não está disponível devido a restrições de espaço.';
