e8cf00a0b770c11323eb84e151093b47aad5b33d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-04-24 10:19:44 +0200

    Update el_GR, thanks to dimitri

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Strike'] = 'Διαγραφή';
+$lang['Sub / Up'] = 'Κάτω/Πάνω';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = 'Η διαμόρφωση %s είναι απενεργοποιημένη. Για να χρησιμοποιήσετε τον EditorPlus πρέπει να ενεργοποιήσετε αυτή τη διαμόρφωση.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Ο συντάκτης φαίνεται έτσι παντού στο Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Για να το κάνετε αυτό, τραβήξτε και μεταφέρετε στοιχεία πάνω στην ελαχιστοποιημένη εργαλειοθήκη.';
+$lang['Type something here'] = 'Πληκτρολογήστε κάτι εδώ';
+$lang['Underline'] = 'Υπογράμμιση';
+$lang['Video'] = 'Βίντεο';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Προειδοποίηση: Αν αλλάξετε συντάκτη, μερικά τμήματα μορφοποίησης δεν θα είναι συμβατά, που μπορεί να έχει σαν αποτέλεσμα να χαθεί η διαμόρφωση στο περιεχόμενό σας.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'Με την λειτουργία ελαχιστοποίησης (μικρή απεικόνιση), μπορείτε να επιλέξετε λίγα στοιχεία για να τα προσθέσετε στην εργαλειοθήκη.';
+$lang['Show/Hide Preview'] = 'Δείξε/Κρύψε την προεπισκόπηση';
+$lang['Sandbox'] = 'Περιβάλλον ασφαλούς εκτέλεσης';
+$lang['Quote'] = 'Φράση';
+$lang['Minify Toolbar (drag items here)'] = 'Ελαχιστοποιημένη Εργαλειοθήκη (μεταφέρετε τα στοιχεία εδώ)';
+$lang['Link'] = 'Σύνδεσμος';
+$lang['Align'] = 'Ευθυγράμμιση';
+$lang['Background color'] = 'Χρώμα φόντου';
+$lang['Bold'] = 'Έντονα';
+$lang['Choose an editor'] = 'Επιλέξτε συντάκτη';
+$lang['Clean'] = 'Καθαρό';
+$lang['Color'] = 'Χρώμα';
+$lang['Direction'] = 'Κατεύθυνση';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Λόγω περιορισμών χώρου δεν μπορείτε να τα επιλέξετε όλα.';
+$lang['EPImage'] = 'Εικόνα';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Επεκτείνετε/συρρικνώστε τον συντάκτη';
+$lang['Font'] = 'Γραμματοσειρά';
+$lang['Font size'] = 'Μέγεθος γραμματοσειράς';
+$lang['Heading'] = 'Επικεφαλίδα';
+$lang['Heading list'] = 'Κατάλογος επικεφαλίδων';
+$lang['Here you can play with it and see its features'] = 'Εδώ μπορείτε να πειραματιστείτε με αυτόν και να δείτε τα χαρακτηριστικά του';
+$lang['Italic'] = 'Πλάγια';
+$lang['Item is active in minify toolbar.'] = 'Το στοιχείο είναι ενεργό στην ελαχιστοποιημένη εργαλειοθήκη';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Το στοιχείο είναι διαθέσιμο για μεταφορά στην ελαχιστοποιημένη εργαλειοθήκη';
+$lang['Item is not available due to space restriction.'] = 'Το στοιχείο δεν είναι διαθέσιμο λόγω περιορισμένου χώρου';
