9a996ff26a918fb6fe892ee0892ccff6c1ed1283
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-12-14 07:17:59 +0100

    Update cs_CZ, thanks to webprostor

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Strike'] = 'Vyškrtnout';
+$lang['Minify Toolbar (drag items here)'] = 'Zmenšená nástrojová lišta (přetáhněte položky sem)';
+$lang['Due to the space restrictions you can\'t choose them all.'] = 'Z důvodu omezení místa nelze provést výběr všech.';
+$lang['Item is available for dragging in the minify toolbar.'] = 'Položka je dostupná pro přetažení do zmenšeného panelu nástrojů.';
+$lang['Link'] = 'Odkaz';
+$lang['Quote'] = 'Citace';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = 'Sandbox';
+$lang['Show/Hide Preview'] = 'Zobrazit/Skrýt Náhled';
+$lang['Sub / Up'] = 'Pod / Nad';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = 'Nastavení %s je deaktivované. Pro použití EditorPlus musíte aktivovat toto nastavení.';
+$lang['This is what the editor looks like throughout Piwigo.'] = 'Takto vypadá editor v Piwigo.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = 'Chcete-li tak učinit, přetáhněte položky do zmenšovacího nástroje.';
+$lang['Type something here'] = 'Zde něco napište.';
+$lang['Underline'] = 'Podtrženo';
+$lang['Video'] = 'Video';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = 'Varování: Pokud změníte editor, některé formátovací prvky nebudou kompatibilní, což může vést ke ztrátě stylů ve vašem textu.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'S EditorPlus mini modem (malé zobrazení), můžete zvolit několik položek pro přidání do nástrojové lišty.';
+$lang['Align'] = 'Zarovnání';
+$lang['Background color'] = 'Barva pozadí';
+$lang['Bold'] = 'Tučně';
+$lang['Choose an editor'] = 'Výběr editoru';
+$lang['Clean'] = 'Odstranit';
+$lang['Color'] = 'Barva';
+$lang['Direction'] = 'Směr';
+$lang['EPImage'] = 'Obrázek';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = 'Rozšířit/Zmenšit';
+$lang['Font'] = 'Písmo';
+$lang['Font size'] = 'Velikost písma';
+$lang['Heading'] = 'Nadpis';
+$lang['Heading list'] = 'Seznam nadpisů';
+$lang['Here you can play with it and see its features'] = 'Zde si vyzkoušejte všechny vlastnosti';
+$lang['Italic'] = 'Italic';
+$lang['Item is active in minify toolbar.'] = 'Položka je aktivní ve zmenšeném panelu nástrojů.';
+$lang['Item is not available due to space restriction.'] = 'Položka není k dispozici z důvodu omezení místa.';
