ccdc9e62bc92a6f5cd2e9063b8fb1b7241a7b845
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 06:57:21 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Link'] = '링크';
+$lang['Minify Toolbar (drag items here)'] = '축소 도구 모음 (항목을 여기로 드래그)';
+$lang['Quote'] = '인용';
+$lang['RTL'] = 'RTL';
+$lang['Sandbox'] = '샌드박스';
+$lang['Show/Hide Preview'] = '미리 보기 표시/숨기기';
+$lang['Strike'] = '취소선';
+$lang['Sub / Up'] = '아래/위';
+$lang['The configuration %s is disabled. To use EditorPlus you must enable this configuration.'] = '%s 설정이 비활성화되었습니다. EditorPlus를 사용하려면 이 설정을 활성화해야 합니다.';
+$lang['This is what the editor looks like throughout Piwigo.'] = '이것이 Piwigo 전체에서 편집기의 모습입니다.';
+$lang['To do this, drag and drop items into the minify toolbar.'] = '이를 위해 항목을 축소 도구 모음으로 드래그 앤 드롭하세요.';
+$lang['Type something here'] = '여기에 입력하세요';
+$lang['Underline'] = '밑줄';
+$lang['Video'] = '동영상';
+$lang['Warning: If you change editor, some formatting elements will not be compatible, which may result in a loss of style in your content.'] = '경고: 편집기를 변경하면 일부 서식 요소가 호환되지 않아 콘텐츠의 스타일이 손실될 수 있습니다.';
+$lang['With EditorPlus minify mode (small display), you can choose a few items to add to the toolbar.'] = 'EditorPlus 축소 모드(소형 표시)에서는 도구 모음에 추가할 몇 가지 항목을 선택할 수 있습니다.';
+$lang['Align'] = '정렬';
+$lang['Background color'] = '배경색';
+$lang['Bold'] = '굵게';
+$lang['Choose an editor'] = '편집기 선택';
+$lang['Clean'] = '지우기';
+$lang['Color'] = '색상';
+$lang['Direction'] = '방향';
+$lang['Due to the space restrictions you can\'t choose them all.'] = '공간 제한으로 인해 모두 선택할 수 없습니다.';
+$lang['EPImage'] = '이미지';
+$lang['EditorPlus'] = 'EditorPlus';
+$lang['Expand/Shrink Editor'] = '편집기 확장/축소';
+$lang['Font'] = '글꼴';
+$lang['Font size'] = '글꼴 크기';
+$lang['Heading'] = '제목';
+$lang['Heading list'] = '목록 제목';
+$lang['Here you can play with it and see its features'] = '여기서 직접 사용해보고 기능을 확인해 보세요';
+$lang['Italic'] = '이탤릭';
+$lang['Item is active in minify toolbar.'] = '항목이 축소 도구 모음에서 활성화되었습니다.';
+$lang['Item is available for dragging in the minify toolbar.'] = '항목이 축소 도구 모음에서 드래그할 수 있습니다.';
+$lang['Item is not available due to space restriction.'] = '항목이 공간 제한으로 사용할 수 없습니다.';
